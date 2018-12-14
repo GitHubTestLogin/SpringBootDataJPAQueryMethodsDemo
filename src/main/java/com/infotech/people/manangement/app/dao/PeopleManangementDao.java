1145a98b9caa9ef1e7711952e045fdbdf0ba087d
Pawan <pawan.km.m@gmail.com>
2018-12-14 13:20:42 +0530

    First Commit

---
+++ src/main/java/com/infotech/people/manangement/app/dao/PeopleManangementDao.java
@@ -0,0 +1,14 @@
+package com.infotech.people.manangement.app.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+
+import com.infotech.people.manangement.app.entities.Person;
+
+public interface PeopleManangementDao extends CrudRepository<Person, Integer>{
+	//public interface PeopleManangementDao extends Repository<Person, Integer>{
+	List<Person> findByLastName(String lastName);
+	List<Person> findByFirstNameAndEmail(String firstName,String email);
+}
